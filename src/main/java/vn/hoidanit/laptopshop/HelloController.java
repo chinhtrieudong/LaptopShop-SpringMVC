6ee8145fdf96c69f8790b1dbe869be9cd5b1f80b
chinh <lebachinh2002@gmail.com>
2024-03-20 00:11:44 +0700

    Setup Mysql với Spring

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -9,4 +9,14 @@ public class HelloController {
     public String index() {
         return "Hello World";
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "User page.";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Admin page.";
+    }
 }
