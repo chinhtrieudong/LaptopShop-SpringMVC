09208551078c7472f0381b1cca3db111037ea4ff
chinh <lebachinh2002@gmail.com>
2024-03-21 20:12:36 +0700

    Integrate Bootstrap & JQuery - config path CSS

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,20 +11,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UserController {
 
-    @RequestMapping
-    public String getHomePage() {
-        return "eric.html";
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("Chinh", "from controller with model");
+        return "hello";
     }
 }
 
 // @RestController
 // public class UserController {
 // private UserService userService;
-
 // public UserController(UserService userService) {
 // this.userService = userService;
 // }
-
 // @GetMapping("/")
 // public String getHomePage() {
 // return userService.handleHello();
